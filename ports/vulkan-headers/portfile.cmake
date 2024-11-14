97ec1b1dfe83a0ebc8dab01caa58bc15a3b33f22
TechSpace <techspace.minecraft@gmail.com>
2024-11-14 21:55:42 +0100

    Update portfile.cmake

--- ports/vulkan-headers/portfile.cmake
+++ ports/vulkan-headers/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO KhronosGroup/Vulkan-Headers
     REF "vulkan-sdk-${VERSION}"
-    SHA512 475cd0c3249c7998e5e85390651a6652e229d65e42adf3b4d18f9f7509037d217e87de01632c9b4142d26ab295cd2272c2e88f897a5420d693a82ff7e4003157
+    SHA512 0
     HEAD_REF main
 )
 
@@ -10,7 +10,7 @@ set(VCPKG_BUILD_TYPE release) # header-only port
 
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DVULKAN_HEADERS_ENABLE_MODULE=OFF
+        -DVULKAN_HEADERS_ENABLE_MODULE=ON
         -DVULKAN_HEADERS_ENABLE_TESTS=OFF
 )
 vcpkg_cmake_install()
